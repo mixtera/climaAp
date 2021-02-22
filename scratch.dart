d42c40195bc0e0f81840f3dbefd198470b5016f8
mixtera <abdukadirovashurmuhammad@gmail.com>
2021-02-22 20:22:40 +0500

    first commit

---
+++ scratch.dart
@@ -0,0 +1,32 @@
+// export 'dart:ui' show Brightness;
+// import 'dart:ui';
+//
+// import 'package:flutter/services.dart';
+//
+// void main() {
+//   performTasks();
+// }
+//
+// void performTasks() {
+//   task1();
+//   task2();
+//   task3();
+// }
+//
+// void task1() {
+//   String result = 'task 1 data';
+//   print('Task 1 complete');
+// }
+//
+// void task2() {
+//   Duration threeSeconds = Duration(seconds: 3);
+//   Future.delayed(threeSeconds, () {
+//     String result = 'task 2 data';
+//     print('Task 2 complete');
+//   });
+// }
+//
+// void task3() {
+//   String result = 'task 2 data';
+//   print('Task 2 complete');
+// }
